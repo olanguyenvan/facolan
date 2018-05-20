32af540613de7d5f305420bb022c7564ef438dde
Ola NguyenVan <olanguyenvan@gmail.com>
2018-05-20 21:10:20 +0200

    kobsy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+
+const MyMapComponent = withScriptjs(withGoogleMap((props) =>
+    <GoogleMap
+        defaultZoom={8}
+        defaultCenter={{ lat: 50.692241, lng: 21.717988 }}
+    >
+        {props.isMarkerShown && <Marker position={{ lat: 50.692241, lng: 21.717988 }} />}
+    </GoogleMap>
+));
 
 class App extends Component {
   render() {
@@ -13,6 +23,13 @@ class App extends Component {
         <p className="App-intro">
           hej, Olnique
         </p>
+          <MyMapComponent
+              isMarkerShown
+              googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+              loadingElement={<div style={{ height: `100%` }} />}
+              containerElement={<div style={{ height: `400px` }} />}
+              mapElement={<div style={{ height: `100%` }} />}
+          />
       </div>
     );
   }
